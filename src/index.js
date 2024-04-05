943b8ef8c321f42ea149724a51771f900808c7d6
Jorge Tinoco <volkney@Jorges-Air-2.lan>
2024-04-05 00:25:54 -0500

    added loop function for player base interaction

--- src/index.js
+++ src/index.js
@@ -1,20 +1,38 @@
 import _ from 'lodash';
 import './css/style.css';
-import Icon from './images/icon.png'
+import Gameboard from './classes/gameboard.js';
+import Player from './classes/player.js'
+import Computer from './classes/computer.js'
+import gameLoop from './loop.js'
 
 
- function component() {
-   const element = document.createElement('div');
+const player = new Player(new Gameboard(10));
+const computer = new Computer(new Gameboard(10));
 
-  // Lodash, now imported by this script
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-   element.classList.add('hello');
 
-     // Add the image to our existing div.
-    const myIcon = new Image();
-    myIcon.src = Icon;
-    element.appendChild(myIcon);
-    return element;
- }
 
- document.body.appendChild(component());
+// Creating div element for game board
+const $gameBoard = document.createElement('div')
+$gameBoard.id = 'game-board'
+document.body.appendChild($gameBoard)
+const gameBoardElement = document.getElementById('game-board')
+
+function drawGameBoard(gameboard, container) {
+  container.innerHTML = '';
+
+  for (const coordinate of Object.keys(gameboard.grid)) {
+    const cell = document.createElement('div');
+    cell.classList.add('game-cell');
+
+    if (gameboard.grid[coordinate].occupied) {
+      cell.classList.add('occupied');
+    } else if (gameboard.grid[coordinate].destroyed) {
+      cell.classList.add('destroyed');
+    }
+
+    cell.textContent = coordinate;
+    container.appendChild(cell);
+  }
+}
+
+gameLoop(drawGameBoard);
--- src/loop.js
+++ src/loop.js
@@ -1,90 +1,32 @@
-class Game {
-    constructor() {
-      this.playerBoard = new Gameboard(10); 
-      this.computerBoard = new Gameboard(10); 
-      this.player = new Player(this.playerBoard); 
-      this.computer = new Computer(this.computerBoard); 
-      this.isPlayerTurn = true; 
-    }
-  
-    start() {
-      // Place player's ships (you'll need to implement this)
-      this.placePlayerShips();
-  
-      // Place computer's ships (you'll need to implement this)
-      this.placeComputerShips();
-  
-      // Start the game loop
-      this.gameLoop();
-    }
-  
-    gameLoop() {
-      // Check if the game is over
-      if (this.isGameOver()) {
-        this.displayResult(); // Display the result (win or lose)
-        return; // Exit the game loop
-      }
-  
-      // If it's the player's turn
-      if (this.isPlayerTurn) {
-        // Get the player's move (you'll need to implement this)
-        const playerMove = this.getPlayerMove();
-  
-        // Player attacks the computer's board
-        this.player.attackOpponent(playerMove, this.computerBoard);
-  
-        // Switch to the computer's turn
-        this.isPlayerTurn = false;
-      } else {
-        // Computer attacks the player's board
-        this.computer.attackOpponent(this.playerBoard);
-  
-        // Switch to the player's turn
-        this.isPlayerTurn = true;
-      }
-  
-      // Continue the game loop
-      this.gameLoop();
-    }
-  
-    isGameOver() {
-      // Check if all player's ships are sunk
-      const playerShipsSunk = this.playerBoard.allShipsSunk();
-  
-      // Check if all computer's ships are sunk
-      const computerShipsSunk = this.computerBoard.allShipsSunk();
-  
-      // If either player's or computer's ships are all sunk, the game is over
-      return playerShipsSunk || computerShipsSunk;
-    }
-  
-    displayResult() {
-      // Check if the player won or lost
-      const playerWon = this.computerBoard.allShipsSunk();
-  
-      // Display the result (win or lose) to the user
-      if (playerWon) {
-        console.log('You won! All computer ships have been sunk.');
-      } else {
-        console.log('You lost! All your ships have been sunk.');
-      }
-    }
-  
-    // You'll need to implement these methods
-    placePlayerShips() {
-      // Code to let the player place their ships on the gameboard
-    }
-  
-    placeComputerShips() {
-      // Code to randomly place the computer's ships on its gameboard
-    }
-  
-    getPlayerMove() {
-      // Code to get the player's move (e.g., via user input)
-      // Return the coordinate as a string (e.g., 'A1')
+export default function gameLoop(drawGameBoard) {
+  // Check if the game is over
+  if (player.gameboard.allShipsSunk()) {
+    console.log("Computer wins!");
+    return;
+  } else if (computer.gameboard.allShipsSunk()) {
+    console.log("Player wins!");
+    return;
+  }
+
+  // Player's turn
+  let validCoordinate = false;
+  while (!validCoordinate) {
+    const playerCoordinate = prompt("Enter your attack coordinates (e.g., A5):");
+    if (computer.gameboard.isValidCoordinate(playerCoordinate)) {
+      player.attackOpponent(playerCoordinate, computer.gameboard);
+      validCoordinate = true;
+    } else {
+      console.log("Invalid coordinate. Please try again.");
     }
   }
-  
-  // Create a new Game instance and start the game
-  const newGame = new Game();
-  newGame.start();
+
+  // Computer's turn
+  computer.attackOpponent(player.gameboard);
+
+  // Redraw the game boards
+  drawGameBoard(player.gameboard, document.getElementById('player-board'));
+  drawGameBoard(computer.gameboard, document.getElementById('computer-board'));
+
+  // Recursively call the game loop
+  requestAnimationFrame(() => gameLoop(drawGameBoard));
+}
